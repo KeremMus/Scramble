3804ab0d06509bb6556b0f78ea8c861520c96dc6
KeremMus <keremcancu42@gmail.com>
2023-09-18 23:45:06 +0300

    Initial Commit

---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Kerem Can Bakır on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
